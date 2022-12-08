b8ead8ad782734b8e15c6a005d53e47e28b57344
YangXuan3 <yxuan1@my.centennialcollege.ca>
2022-12-08 15:27:09 -0500

    2022/12/08_1

--- server/config/app.js
+++ server/config/app.js
@@ -107,12 +107,14 @@ app.use('/answer', answerRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
